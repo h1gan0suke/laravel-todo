643dbd6bc554f102cc45006f739a8f1d541423c3
h1gan0suke <ontei.ssr@gmail.com>
2024-08-05 01:25:40 +0900

    aaaaaaaaaaaaaaaa

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/log-test', function () {
+    Log::info('Log test message');
+    return 'Log test complete';
 });
